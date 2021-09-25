5b323883d9e790ff76bdbf07985fd5bc91269b9d
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-25 18:00:28 -0400

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div id='head_container'>
+            <h1 id='head'> Recipe Recommender</h1>
+       </div> 
+    );
+}
+export default Header;
