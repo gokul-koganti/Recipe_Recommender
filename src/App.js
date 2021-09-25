54e7756eb0bedeb66b82b782c33c96ee2a1b94be
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-25 17:59:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import  Form from './components/Form.js';
-function App() {
+import Header from './components/Header';
+import React,{Component} from 'react';
+/*function App() {
+  
   return (
     <div className="App">
-      <Form/>
+      <Form />
     </div>
   );
+}*/
+class App extends Component{
+
+  
+    constructor(){
+      super()
+      this.state={
+          cuisine : "Any",
+          min_cal : "",
+          max_cal : "",
+          ingredients: new Set()  
+        }
+      }
+    formGetter = (cuisine, minCal, maxCal, ing) => {
+      this.setState({
+        cuisine : cuisine,
+          min_cal : minCal,
+          max_cal : maxCal,
+          ingredients: ing 
+      })
+    }
+
+  render(){
+    return(
+    <div>
+    <Header/>
+    <Form sendFormData = {this.formGetter}/>
+      <p> cuisine: {this.state.cuisine}</p>
+      <p> max: {this.state.max_cal}</p>
+      <p> min: {this.state.min_cal}</p>
+      <p> ingredients: {this.state.ingredients}</p>
+    </div>
+    )
+  }
 }
 
 export default App;
