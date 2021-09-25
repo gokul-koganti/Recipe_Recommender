01d7539b8e1268f960ecdc87258984130efc4e04
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-25 17:58:49 -0400

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,19 @@
 import React, {Component} from "react";
 
 class Form extends Component{
+   
     constructor(){
         super()
         this.state={
             cuisine : "Any",
             min_cal : "",
             max_cal : "",
-            ingredients: new Set()
-            
+            ingredients: new Set()  
             
         }
-
     }
 
+   
     minUpdate = (event) =>{
         this.setState({
             cuisine : this.state.cuisine,
@@ -24,6 +24,8 @@ class Form extends Component{
         },()=>console.log(this.state))
     }
 
+    
+    
     maxUpdate = (event) =>{
         this.setState({
             cuisine : this.state.cuisine,
@@ -34,6 +36,7 @@ class Form extends Component{
         },()=>console.log(this.state))
     }
 
+    
     cuisineUpdate = (event) =>{
         this.setState({
             cuisine : event.target.value,
@@ -43,17 +46,45 @@ class Form extends Component{
         
         },()=>console.log(this.state))
     }
-    addHandler = ()=> {
+    
+    
+    addHandler = (event)=> {
         const ingredient = document.getElementById("ingredient").value
         console.log(ingredient)
-       this.setState({
+        this.setState({
             cuisine : this.state.cuisine,
             min_cal : this.state.min_cal,
             max_cal : this.state.max_cal,
             ingredients:new Set(this.state.ingredients).add(ingredient)
         
-        },()=>console.log(this.state.ingredients))
+        },()=>console.log(this.state))
     }
+
+    
+    removeHandler = () => {
+        var discardIngredient = window.prompt("Enter the item to be discarded: ");
+        var ingredientList = this.state.ingredients
+        ingredientList.delete(discardIngredient)
+        this.setState({
+            cuisine : this.state.cuisine,
+            min_cal : this.state.min_cal,
+            max_cal : this.state.max_cal,
+            ingredients: ingredientList
+        
+        },()=>console.log(this.state))
+
+    }
+    
+    handleSubmit = (event) => {
+        this.props.sendFormData(this.state.cuisine, this.state.min_cal,this.state.max_cal,this.state.ingredients)
+        // alert(this.state.max_cal)
+        // console.log(this.state.ingredients)
+    event.preventDefault()
+        
+    }
+
+   
+
     render(){
         const cuisine_list = ["Any", "Mexican", "Swedish", "Latvian", "Italian", "Spanish", "American","Scottish","British","Thai",
         "Japanese","Chinese","Indian","Canadian","Russian","Jewish","Polish","German","French","Hawaiian","Brazilian", 
@@ -62,25 +93,28 @@ class Form extends Component{
         return(
             <div>
 
-                <form>
-                    <label><h3>Ingredient List</h3></label><br/>
-                   
-                    <div>
+                <form onSubmit={this.handleSubmit}>
                     
-                    <input type = "text" id = "ingredient" /> <button onClick ={this.addHandler} type="button" > Add item</button><button> Remove item</button><br/><br/>
-                    
-
                    
+                    <div id='FormContainer1'>
+                    <label id='IngredientLabel'>Ingredients</label><br/>
+                    <label class='sideLabel'> Ingredient: </label>
+                     <input type = "text" id = "ingredient" /> 
+                     <button onClick ={this.addHandler} type="button" > Add item</button>
+                     <button onClick ={this.removeHandler} type="button"> Remove item</button><br/><br/>
                     </div>
                     
 
-                    <div>
-                    <label> <b>Calorie Filter</b> </label><br/><br/>
-                    <label> Minimum Calories: </label> <input type = "text" value={this.state.min_cal} onChange={this.minUpdate}/>
-                    <label> Maximum Calories: </label> <input type = "text" value={this.state.max_cal} onChange={this.maxUpdate}/>
+                    <div id = 'FormContainer2'>
+                    <label id ='CalorieLabel'> <b>Calorie Filter</b> </label><br/><br/>
+                    <label class='sideLabel'> Minimum Calories: </label> 
+                    <input type = "text" value={this.state.min_cal} onChange={this.minUpdate}/><br/>
+                    <label class='sideLabel'> Maximum Calories: </label>
+                     <input type = "text" value={this.state.max_cal} onChange={this.maxUpdate}/><br/>
                     </div>
 
-                    <div>
+                    <div id = 'FormContainer3'>
+                    <label class='sideLabel'> Cusine Selection: </label>
                     <select value={this.state.cuisine} onChange={this.cuisineUpdate} >{
                         cuisine_list.map((category,i) => <option key={i}>{category}</option>)
                     }
@@ -88,7 +122,7 @@ class Form extends Component{
                     </div>
 
 
-                <button type="submit"> <h4>Search Recipes</h4> </button>
+                <button type="button" id='submit' onClick={this.handleSubmit}  > <h4>Search Recipes</h4> </button>
                 </form>
             </div>
         )
