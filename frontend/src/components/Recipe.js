eaac5c622e1f5ae919ee70c1c7306102125b30a7
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-29 01:12:34 -0400

    Update Recipe.js

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -4,21 +4,24 @@ import Card from 'react-bootstrap/Card'
 import VideoURL from './VideoURL';
 
 const Recipe = (recipe) => {
-   
+    var ingredients_seperated = recipe.recipe['Cleaned-Ingredients'].split(',');
+    ingredients_seperated = ingredients_seperated.map((ingredient) => <li class= "recipe_ingredients" > {ingredient}</li>);
     return (
-            <div className='ui embed'>
-                <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={recipe.recipe['image-url']} alt={recipe.recipe.TranslatedRecipeName} width="100px" height="100px"/>
-                <Card.Body>
-                    <Card.Title>{recipe.recipe.TranslatedRecipeName}</Card.Title>
-                    <Card.Text> Ingredients:
-                        {recipe.recipe['Cleaned-Ingredients']}
-                    </Card.Text>
-                 </Card.Body>
+                
+
+                <tr id = "resultContainer" style={{ width: '18rem' }}>
+                <img  src={recipe.recipe['image-url']} alt={recipe.recipe.TranslatedRecipeName} />
+                <body>
+                   <label> <b>{recipe.recipe.TranslatedRecipeName}</b> </label><br/><br/>
+                    <label><b> Ingredients:</b> </label><br/>
+                    
+                       <ul id="result_unordered"> {ingredients_seperated} </ul>
+                    
+                    
+                 </body>
                 {/* <VideoURL getRecipeVideo={recipe.recipe.TranslatedRecipeName}/> */}
-            </Card>    
-            </div>
+            </tr>   
+            
+            
     )
     };
-
-export default Recipe;
