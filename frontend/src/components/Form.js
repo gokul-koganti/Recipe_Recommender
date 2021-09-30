75be46f9c4224ec965f68547560b7fe1ad82a48f
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-30 17:44:55 -0400

    Update Form.js
    Changed naming conventions and added comments

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,113 +1,159 @@
 import React, {Component} from "react";
 
-//Form Component task is to take user inputs, process inputs and send it back to App Component
+// Form component to maintain input form
 class Form extends Component{
    
+    // constructor for Form Component
+    // We maintain user input as a state object
     constructor(){
+
         super()
-        this.state={
+
+        this.state = {
+
             cuisine : "Any",
-            numberIngredients: 0,
-            ingredients: new Set()             
+            numberIngredients : 0,
+            ingredients : new Set()  
+
         }
+
     }
 
-    //callback function on updation of cuisine input
-    cuisineUpdate = (event) =>{
-        this.setState({
-            cuisine : event.target.value,
-            numberIngredients : this.state.numberIngredients,
-            ingredients : this.state.ingredients
-        
-        },()=>console.log(this.state))
+    // function to change cuisine state, triggered on selection of a cuisine item.
+    cuisineUpdate = (event) => {
+
+        this.setState (
+            {
+                cuisine : event.target.value,
+                numberIngredients : this.state.numberIngredients,
+                ingredients : this.state.ingredients
+            }, () => console.log( this.state ) 
+        )
     }
 
-    //callback function on updation of number of ingredients input
-    numberUpdate = (event) =>{
-        this.setState({
-            cuisine : this.state.cuisine,
-            numberIngredients: event.target.value,
-            ingredients:this.state.ingredients
-        
-        },()=>console.log(this.state))
+    // function to update the maximum number of ingredients in the state.
+    numberUpdate = (event) => 
+    {
+        this.setState (
+            {
+                cuisine : this.state.cuisine,
+                numberIngredients: event.target.value,
+                ingredients:this.state.ingredients
+            }, () => console.log( this.state )
+        )
     }
     
-    //functions that handles printing and removal of ingredients 
-    printHander =() => {
-        for (const item of this.state.ingredients) {
-            console.log(1,item)
-          }
+    // function to display the ingredients added by the user upto that point.
+    printHander = () => 
+    {
+        // converting the set into an array, to make it iterable
         const items = [...this.state.ingredients]
-        const list_items = items.map((item) => <li onMouseDown={this.removeHandler} id={item}> {item}</li>);
-       return(<ul>{list_items}</ul>);
+
+        // mapping each item to be displayed as a list item
+        const list_items = items.map((item) => <li onMouseDown={this.removeHandler} id={item} class= "addedIngredient"> {item}</li>);
+
+       return(<ul class = "addedIngredientList">{list_items}</ul>);
+
     }
 
-    //callback function that handles addition of ingredients
-    addHandler = (event)=> {
+    // fucntion to add ingredients to the inredients (set datastructure) in App's state
+    // triggered by clicking add item button
+    addHandler = (event) => 
+    {
+       
         const ingredient = document.getElementById("ingredient").value
-        this.setState({
-            cuisine : this.state.cuisine,
-            numberIngredients : this.state.numberIngredients,
-            ingredients:new Set(this.state.ingredients).add(ingredient)
+
+        this.setState (
+            {
+                cuisine : this.state.cuisine,
+                numberIngredients : this.state.numberIngredients,
+                ingredients : new Set( this.state.ingredients ).add( ingredient )
         
-        })
+            }, () => console.log( this.state )
+        )
+        
         document.getElementById("ingredient").value = ""
+
     }
 
-    //callback function that handles removal of ingredients
-    removeHandler = (event) => {
+    // fucntion to add ingredients to the inredients (set datastructure) in App's state
+    // triggered by clicking item that is displayed ysing printHandler function
+    removeHandler = (event) => 
+    {
         var discardIngredient = event.target.id
         var ingredientList = this.state.ingredients
-        ingredientList.delete(discardIngredient)
-        this.setState({
-            cuisine : this.state.cuisine,
-            numberIngredients : this.state.numberIngredients,
-            ingredients: ingredientList
         
-        },()=>console.log(this.state))
+        ingredientList.delete(discardIngredient)
+
+        this.setState (
+            {
+                cuisine : this.state.cuisine,
+                numberIngredients : this.state.numberIngredients,
+                ingredients : ingredientList
+        
+            }, () => console.log( this.state )
+        )
+
     }
     
-    handleSubmit = (event) => {
-        event.preventDefault();
+    // function to send the data to the parent App component
+    // uses the function that is sent through props from the App Component
+    handleSubmit = (event) => 
+    {
+
+        event.preventDefault();        
         this.props.sendFormData(this.state.cuisine, this.state.numberIngredients,this.state.ingredients)
+
     }
 
+    // render function dispays the UI content i.e the form content
     render(){
-        const cuisine_list = ["Any", "Mexican", "Swedish", "Latvian", "Italian", "Spanish", "American","Scottish","British","Thai",
-        "Japanese","Chinese","Indian","Canadian","Russian","Jewish","Polish","German","French","Hawaiian","Brazilian", 
-        "Peruvian","Cuban","Tibetian","Salvadorian","Egyptian","Greek","Belgian","Irish","Welsh","Mormon","Cajun","Portugese",
-    "Turkish","Haitian","Tahitian","Kenyan","Korean","Algerian","Nigerian","Libyan"]
+
+        const cuisine_list = [ "Any", "Mexican", "Swedish", "Latvian", "Italian",
+        "Spanish", "American","Scottish","British","Thai","Japanese","Chinese",
+        "Indian","Canadian","Russian","Jewish","Polish","German","French","Hawaiian",
+        "Brazilian", "Peruvian","Cuban","Tibetian","Salvadorian","Egyptian","Greek",
+        "Belgian","Irish","Welsh","Mormon","Cajun","Portugese","Turkish","Haitian",
+        "Tahitian","Kenyan","Korean","Algerian","Nigerian","Libyan" ]
+
+        // returns jsx element
         return(
-            <div class="container">
+
+            <div class="formOutercontainer">
 
                 <form onSubmit={this.handleSubmit}>
                     
                     <div id='FormContainer1'>
-                    <label class='sideLabel'> Ingredient: </label><br/>
-                     <input type = "text" id = "ingredient" /> 
-                     <button onClick ={this.addHandler} type="button" id= "addButton" > Add item</button>
-                     
+                        <label class='sideLabel'> Ingredient: </label><br/>
+                        <input type = "text" id = "ingredient" /> 
+                        <button onClick = {this.addHandler} type = "button" id = "addButton" > Add item </button>
                     </div>
-                    <label class='sideLabel'>Added Items:</label>
-                    <div id='FormContainer2'>{this.printHander()}</div>
+
+                    <div id='FormContainer2'>
+                        <label class='sideLabel'>Added Items:</label><br/>
+                        {this.printHander()}
+                    </div>
 
                     <div id='FormContainer3'>
-                    
-                    <label class='sideLabel'> Maximum Number of Ingredients: </label><br/>
-                     <input type = "number" id = "NoIngredient" onChange={this.numberUpdate} /> 
+                        <label class='sideLabel'> Maximum Number of Ingredients: </label><br/>
+                        <input type = "number" id = "NoIngredient" onChange = {this.numberUpdate} /> 
                     </div>
                        
                     <div id = 'FormContainer4'>
-                    <label class='sideLabel'> Cusine Selection: </label><br/>
-                    <select value={this.state.cuisine} onChange={this.cuisineUpdate} >{
-                        cuisine_list.map((category,i) => <option key={i}>{category}</option>)
-                    }
-                    </select>
+                        <label class='sideLabel'> Cusine Selection: </label><br/>
+                        <select value={this.state.cuisine} onChange={this.cuisineUpdate} >
+                            {
+                                cuisine_list.map( (category, i) => <option key = {i}> {category} </option> )
+                            }
+                        </select>
                     </div>
 
-                 <div id = 'FormContainer5'>
-                    <button type="button" id='submit' onClick={this.handleSubmit}  > <h4>Search Recipes</h4> </button>
+                    <div id = 'FormContainer5'>
+                        <button type = "button" id = 'submit' onClick = {this.handleSubmit}> 
+                            <h4> Search Recipes </h4> 
+                        </button>
                     </div>
+
                 </form>
             </div>
         )
