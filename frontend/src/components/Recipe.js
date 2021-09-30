f1c361ad26fd56767387c6a7b6cbebbdea5bdeae
Gokul Krishna Koganti <32881355+gokul-koganti@users.noreply.github.com>
2021-09-30 17:45:46 -0400

    Update Recipe.js
    Added comments and changed few names

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import '../video.css';
+import Card from 'react-bootstrap/Card'
 import VideoURL from './VideoURL';
 
-//  Recipe Component takes details of recipe and styles info like image,ingredients etc accordingly
-const Recipe = (recipe) => {
-    var ingredients_seperated = recipe.recipe['Cleaned-Ingredients'].split(',');
-    ingredients_seperated = ingredients_seperated.map((ingredient) => <li class= "recipe_ingredients" > {ingredient}</li>);
-    return (
-                <tr id = "resultContainer" style={{ width: '18rem' }}>
-                <img  src={recipe.recipe['image-url']} alt={recipe.recipe.TranslatedRecipeName} />
-                <body>
-                   <label> <b>{recipe.recipe.TranslatedRecipeName}</b> </label><br/><br/>
-                    <label><b> Ingredients:</b> </label><br/>
-                    
-                       <ul id="result_unordered"> {ingredients_seperated} </ul>
-                 </body>
-                {/* <VideoURL recipe={recipe.recipe.TranslatedRecipeName}/> */}
-            </tr>   
-    )
+// Recipe component dealing with individial recipe items
+const Recipe = (recipe) => 
+
+    {
+        // splitting the ingredients with seperator as a comma
+        var ingredients_seperated = recipe.recipe['Cleaned-Ingredients'].split(',');
+
+        // mapping each ingredient to be displayes as a list item
+        ingredients_seperated = ingredients_seperated.map( (ingredient) => 
+        <li class= "recipe_ingredient_item" > {ingredient}</li> );
+
+        // returns individual container for each recipe     
+        return (
+                <tr id = "resultContainer" >
+                    <img  src={recipe.recipe['image-url']} alt={recipe.recipe.TranslatedRecipeName} />
+                    <body>
+                        <label  class="sideLabel"> <b>{recipe.recipe.TranslatedRecipeName}</b> </label><br/><br/>
+                        <label class="sideLabel"><b> Ingredients:</b> </label><br/>
+                        <ul class="result_ingredients"> {ingredients_seperated} </ul>
+                    </body>
+                </tr>   
+            
+        )
     };
+
 export default Recipe;
+
